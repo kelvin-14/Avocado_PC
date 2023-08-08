08f13d89ec0ab497fc7ddaf9be12c87c329885c3
23knn <129178453+23knn@users.noreply.github.com>
2023-08-08 21:13:58 +0300

    launch tray made as a substitute for Windows Notification Service. can now quit from the tray and relaunch from there. having the launch tray allows us to send notifications since without it, we'd need to use the WNS since chromium cant send notifications when closed

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, ipcRenderer } = require('electron');
+const { app, BrowserWindow, ipcMain, ipcRenderer, Tray, Menu } = require('electron');
 const path = require('path');
 const url = require('url');
 function createWindow() {
@@ -27,39 +27,78 @@ function createWindow() {
           }
           
     });
-    console.log(win)
     // if(isDev === 1) {
     //     win.webContents.openDevTools();
     // }
-
     win.loadURL(startUrl);
+    win.on('minimize',function(event){
+        event.preventDefault();
+        win.hide();
+    });
+    
+    win.on('close', function (event) {
+        if(!app.isQuiting){
+            event.preventDefault();
+            win.hide();
+        }
+        return false;
+    });
     return win
-}       
-    const darkenTitleBar = (window) => {
-        window.setTitleBarOverlay(
-            {color:'black', symbolColor:'white', height:30}
-        )
+}   
+
+app.whenReady().then(() => {
+    const window = createWindow()
+
+    ipcMain.handle("darkenTitleBar", () => {darkenTitleBar(window)})
+    ipcMain.handle("lightenTitleBar", () => {lightenTitleBar(window)})
+    
+    createTray()
+
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
     }
+});
 
-    const lightenTitleBar = (window) => {
-        window.setTitleBarOverlay(
-            {color:'#f1f1f1', symbolColor:'black', height:30}
-        )
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
     }
+});
 
 
-    app.whenReady().then(() => {
-        const window = createWindow()
-        ipcMain.handle("darkenTitleBar", () => {darkenTitleBar(window)})
-        ipcMain.handle("lightenTitleBar", () => {lightenTitleBar(window)})
-    });
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            app.quit()
+// HELPERS
+const darkenTitleBar = (window) => {
+    window.setTitleBarOverlay(
+        {color:'black', symbolColor:'white', height:30}
+    )
+}
+
+const lightenTitleBar = (window) => {
+    window.setTitleBarOverlay(
+        {color:'#f1f1f1', symbolColor:'black', height:30}
+    )
+}
+
+let tray = null
+const createTray = () => {
+    tray = new Tray(path.join(__dirname, 'favicon.ico'))
+    
+    tray.setToolTip('Avoca.do')
+    
+    tray.setContextMenu(Menu.buildFromTemplate([
+        {
+            label: "Quit",
+            click: () => {
+                app.isQuiting = true;
+                app.quit();
+            }
         }
-    });
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    });
+    ]))
+
+    tray.on('click', () => {
+        window.show()
+    })
+}
